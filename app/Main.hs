6ac4f21af8654c7f3fba0e171a9719ac58290a40
DzOlha <monsarda11@gmail.com>
2023-12-17 21:09:57 +0200

    Made problems 1, 2, 4, 6, 8.

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,6 @@ module Main where
 
 import GHC.Conc ( numCapabilities, pseq )
 import Data.Time.Clock (diffUTCTime, getCurrentTime)
-import GraphPath (findPaths)
 import Lib
 
 main :: IO ()
@@ -21,7 +20,3 @@ main = do
     end <- getCurrentTime
     putStrLn $ show (end `diffUTCTime` start) ++ " elapsed."
     putStrLn $ "number of cores: " ++ show numCapabilities
-    
--- ---------------------   GraphPth - problem 4 -------------------------------
-    let graph = [[1, 3, 4], [0, 2, 3], [1, 3], [0, 1, 2, 4], [0, 3]]
-        print $ findPaths graph 0 1
